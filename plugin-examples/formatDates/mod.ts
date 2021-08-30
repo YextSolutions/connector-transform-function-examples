72f5fbeb26c9243a46b854065b5c344b5d725789
dfesjian <31452555+dfesjianYext@users.noreply.github.com>
2021-08-30 14:03:02 +0200

    Added another date function

--- plugin-examples/formatDates/mod.ts
+++ plugin-examples/formatDates/mod.ts
@@ -63,3 +63,27 @@ export function formatDate(dateString: string) {
 
     return formattedDate
 }
+
+// Converts Unix Timestamp to Yext Supported Date
+export function formatUnixDate(dateUnix: string) {
+    var formattedDate = ""
+    if (dateUnix != "") {
+        var date = new Date(dateUnix*1000)
+         
+        var year = date.getFullYear()
+        
+        var numMonth = date.getMonth() + 1
+        var month = String(numMonth)
+        if (numMonth < 10) month = '0' + month
+
+        var numDay = date.getDate()
+        var day = String(numDay)
+        if (numDay < 10) day = '0' + day
+
+        formattedDate = year + "-" + month + "-" + day
+
+        console.log(formattedDate)
+    }
+
+    return formattedDate
+}
